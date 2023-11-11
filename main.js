cce11c2e23f67e1dc10caecdc0780c5fabcf4cad
Yu WATANABE <yu_w_main@yahoo.co.jp>
2023-11-11 13:49:19 +0900

    全てのファイルのcommit

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const ham = document.querySelector("#js-hamburger");
+const nav = document.querySelector("#js-nav");
+
+ham.addEventListener('click', function() {
+   ham.classList.toggle('active');
+    nav.classList.toggle('active');
+});
